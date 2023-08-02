c35d140a15ef8bb05e1a748327681af258708744
VelocityVerse <110754714+naresh9919@users.noreply.github.com>
2023-08-02 07:00:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.location
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
-resource "aws_instance" "myInstance" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
-  key_name = "terraform"
+resource "aws_instance" "Jenkins-instance" {
+  ami           = var.os-name
+  instance_type = var.instance-type
+  key_name = var.key-pair
 
   tags = {
     Name = "Jenkins-instance"
