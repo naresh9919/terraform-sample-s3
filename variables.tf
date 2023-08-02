5ba547a7cf77f5b95e98570bfbaecedf19179ea1
VelocityVerse <110754714+naresh9919@users.noreply.github.com>
2023-08-02 06:42:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "access_key" {
+  default = "AKIA6JFVU6E4DYY7K6PT"
+}
+
+variable "secret_key" {
+  default = "HZEKaXS8MhtxZHeWBq1Fz367haylsVqvCQTB5IBl"
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
